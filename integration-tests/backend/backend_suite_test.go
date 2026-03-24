ff52909026ad068dd993463511f04f17937344c4
Oliver Smakal <osmakal@osmakal-thinkpadp1gen7.rmtcz.csb>
2026-03-24 14:01:15 +0100

    add example of beforesuite usage

--- integration-tests/backend/backend_suite_test.go
+++ integration-tests/backend/backend_suite_test.go
@@ -3,10 +3,13 @@ package netobserv
 import (
 	"flag"
 	"testing"
+	"os"
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
+	filePath "path/filepath"
 	exutil "github.com/openshift/origin/test/extended/util"
+	// compat_otp "github.com/openshift/origin/test/extended/util/compat_otp"
 	e2eframework "k8s.io/kubernetes/test/e2e/framework"
 )
 
@@ -24,6 +27,36 @@ var _ = BeforeSuite(func() {
 
 	// Initialize test
 	Expect(exutil.InitTest(false)).NotTo(HaveOccurred())
+
+	// example of before suite working - check the noo exists 
+	if os.Getenv("CHECK_NOO_EXISTS") == "TRUE" {
+		baseDir, _ := filePath.Abs("./testdata")
+		subscriptionDir := filePath.Join(baseDir,"subscription")
+		oc := exutil.NewCLIForMonitorTest("netobserv")
+
+		NOcatSrc := Resource{"catsrc", "netobserv-konflux-fbc", netobservNS}
+		NOSource := CatalogSourceObjects{"stable", NOcatSrc.Name, NOcatSrc.Namespace}
+
+
+		NO := SubscriptionObjects{
+			OperatorName:  "netobserv-operator",
+			Namespace:     netobservNS,
+			PackageName:   NOPackageName,
+			Subscription:  filePath.Join(subscriptionDir, "sub-template.yaml"),
+			OperatorGroup: filePath.Join(subscriptionDir, "allnamespace-og.yaml"),
+			CatalogSource: &NOSource,
+		}
+
+		// NetObserv-specific checks only if operator was just deployed
+		NOexisting, err := CheckOperatorStatus(oc, NO.Namespace, NO.PackageName)
+		Expect(err).NotTo(HaveOccurred())
+		Expect(NOexisting).To(BeTrue())
+		// Verify FlowCollector API exists
+		flowcollectorAPIExists, err := isFlowCollectorAPIExists(oc)
+		Expect(flowcollectorAPIExists).To(BeTrue())
+		Expect(err).NotTo(HaveOccurred())
+	}
+
 })
 
 func TestBackend(t *testing.T) {
